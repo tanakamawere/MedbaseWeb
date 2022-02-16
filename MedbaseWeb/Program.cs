a59ff9f27342b35a35676f58042d6e3aa3d37a72
Tanaka Mawere <work@tanakamawere.co.zw>
2022-02-16 20:15:45 +0200

    Thumbnails weren't showing on WhatsApp so I found a provider for image hosting and have implemented that. A tweak to the error page. Hopefully, whenever a page that doesn't work is requested, it will show.

--- MedbaseWeb/Program.cs
+++ MedbaseWeb/Program.cs
@@ -10,7 +10,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
 }
 else
 {
